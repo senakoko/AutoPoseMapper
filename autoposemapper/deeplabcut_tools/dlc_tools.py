3d6446effa3fe05f397fee4460071930383b114c
senaagezo <senaagezo@gmail.com>
2022-09-16 12:36:50 -0400

    added a function to copy the labeled data used to train the DLC network

--- autoposemapper/deeplabcut_tools/dlc_tools.py
+++ autoposemapper/deeplabcut_tools/dlc_tools.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import warnings
 import glob
@@ -43,4 +44,35 @@ class DlcHelper:
             file_s = Path(file).stem
             file_s = file_s[:file_s.find('DLC')]
             destination_file = f'{destination_path}/{file_s}_{self.parameters.conv_tracker_name}.h5'
+            shutil.copy(file, destination_file)
+
+    def copy_labeled_DLC_h5(self, labeled_data_path=None):
+        """
+        Skip this function if you don't have the manually labeled data used to train the DLC network.
+        Copy the labeled h5 files to the autoencoder data path. If you have this data, you will use it to train your
+        autoencoder network the first time.
+        Parameters
+        ----------
+        labeled_data_path: Path to the data used to train the DLC network
+
+        Returns
+        -------
+        """
+
+        if labeled_data_path is None:
+            print("Please provide the path to DLC labeled-data. Skip this function if you don't have the data")
+            return
+
+        h5_files = sorted(glob.glob(f'{labeled_data_path}/**/Coll*.h5', recursive=True))
+
+        destination_path = Path(self.project_path) / self.parameters.autoencoder_data_name
+
+        for file in h5_files:
+            file_s = Path(file).stem
+            file_p = Path(file).parts
+            sub_folder = '/'.join(file_p[-3:-1])
+            sub_folder_path = f'{destination_path}/{sub_folder}'
+            if not os.path.exists(sub_folder_path):
+                os.makedirs(sub_folder_path, exist_ok=True)
+            destination_file = f'{sub_folder_path}/{file_s}_{self.parameters.conv_tracker_name}.h5'
             shutil.copy(file, destination_file)
