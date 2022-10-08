01f3a2897fd7c4ba33d4a7cd05d7b3b317a1eb0e
Sena Agezo <senaagezo@gmail.com>
2022-10-08 17:56:44 -0400

    Added code and notebook to calculate the body centroid and add it to the h5 file

---
+++ autoposemapper/auxiliary_tools/addBodyCenter.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def add_body_center(file):
+
+    # Including the 'midBody' in the old list of bodyparts
+    new_bodyparts = ['Nose', 'leftEar', 'betweenEars', 'rightEar', 'rightMidWaist', 'midBody',
+                     'leftMidWaist', 'leftHip', 'midHip', 'rightHip', 'tailStart']
+
+    with pd.HDFStore(file) as df:
+        animal_key = df.keys()[0][1:]
+        h5 = df[animal_key]
+
+    scorer = h5.columns.get_level_values('scorer').unique().item()
+    individuals = h5.columns.get_level_values('individuals').unique()
+
+    # Create new h5 file with the body parts to be kept
+    main_data = pd.DataFrame()
+    for ind in individuals:
+        data_ind = h5[scorer][ind]
+        bodypart = h5[scorer][ind].loc[:, ['rightMidWaist', 'leftMidWaist']]
+        center = bodypart.rightMidWaist.add(bodypart.leftMidWaist).divide(2)
+        col_center = pd.MultiIndex.from_product([['midBody'], ['x', 'y']], names=['bodyparts', 'coords'])
+        center_df = pd.DataFrame(center.values, index=center.index, columns=col_center)
+        new_data = pd.concat([data_ind, center_df], axis=1)
+
+        # Rearrange the column names in the new h5 file
+        rearranged_data = pd.DataFrame()
+        for bpts in new_bodyparts:
+            rearranged_data = pd.concat([rearranged_data, new_data[bpts]], axis=1)
+        main_data = pd.concat([main_data, rearranged_data], axis=1)
+
+    # Save processed h5 file
+    col = pd.MultiIndex.from_product([[scorer], individuals, new_bodyparts, ['x', 'y']],
+                                     names=['scorer', 'individuals', 'bodyparts', 'coords'])
+    main_df = pd.DataFrame(main_data.values, index=main_data.index, columns=col)
+    main_df.to_hdf(file, animal_key)
+
+    return main_df
