ec8758315f7da3ff1564d1bd17ebdc221ba02cb1
Sena Agezo <senaagezo@gmail.com>
2022-11-10 17:45:12 -0500

    Updated addBodyCenter.py

--- autoposemapper/auxiliary_tools/addBodyCenter.py
+++ autoposemapper/auxiliary_tools/addBodyCenter.py
@@ -13,27 +13,32 @@ def add_body_center(file):
 
     scorer = h5.columns.get_level_values('scorer').unique().item()
     individuals = h5.columns.get_level_values('individuals').unique()
+    bodyparts = h5.columns.get_level_values('bodyparts').unique()
 
-    # Create new h5 file with the body parts to be kept
-    main_data = pd.DataFrame()
-    for ind in individuals:
-        data_ind = h5[scorer][ind]
-        bodypart = h5[scorer][ind].loc[:, ['rightMidWaist', 'leftMidWaist']]
-        center = bodypart.rightMidWaist.add(bodypart.leftMidWaist).divide(2)
-        col_center = pd.MultiIndex.from_product([['midBody'], ['x', 'y']], names=['bodyparts', 'coords'])
-        center_df = pd.DataFrame(center.values, index=center.index, columns=col_center)
-        new_data = pd.concat([data_ind, center_df], axis=1)
 
-        # Rearrange the column names in the new h5 file
-        rearranged_data = pd.DataFrame()
-        for bpts in new_bodyparts:
-            rearranged_data = pd.concat([rearranged_data, new_data[bpts]], axis=1)
-        main_data = pd.concat([main_data, rearranged_data], axis=1)
+    if 'midBody' not in bodyparts:
+        # Create new h5 file with the body parts to be kept
+        main_data = pd.DataFrame()
+        for ind in individuals:
+            data_ind = h5[scorer][ind]
+            bodypart = h5[scorer][ind].loc[:, ['rightMidWaist', 'leftMidWaist']]
+            center = bodypart.rightMidWaist.add(bodypart.leftMidWaist).divide(2)
+            col_center = pd.MultiIndex.from_product([['midBody'], ['x', 'y']], names=['bodyparts', 'coords'])
+            center_df = pd.DataFrame(center.values, index=center.index, columns=col_center)
+            new_data = pd.concat([data_ind, center_df], axis=1)
 
-    # Save processed h5 file
-    col = pd.MultiIndex.from_product([[scorer], individuals, new_bodyparts, ['x', 'y']],
-                                     names=['scorer', 'individuals', 'bodyparts', 'coords'])
-    main_df = pd.DataFrame(main_data.values, index=main_data.index, columns=col)
-    main_df.to_hdf(file, animal_key)
+            # Rearrange the column names in the new h5 file
+            rearranged_data = pd.DataFrame()
+            for bpts in new_bodyparts:
+                rearranged_data = pd.concat([rearranged_data, new_data[bpts]], axis=1)
+            main_data = pd.concat([main_data, rearranged_data], axis=1)
 
-    return main_df
+        # Save processed h5 file
+        col = pd.MultiIndex.from_product([[scorer], individuals, new_bodyparts, ['x', 'y']],
+                                        names=['scorer', 'individuals', 'bodyparts', 'coords'])
+        main_df = pd.DataFrame(main_data.values, index=main_data.index, columns=col)
+        main_df.to_hdf(file, animal_key)
+
+        return main_df
+    else:
+        print('midBody already added to the file')
